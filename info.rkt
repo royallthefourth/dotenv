effc849c3715270fb832e3829594fa3d59b8fba9
Royall Spence <royall@royall.us>
2017-08-27 13:36:18 -0400

    Add raco package template

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+(define collection "dotenv")
+(define deps '("base"
+               "rackunit-lib"))
+(define build-deps '("scribble-lib" "racket-doc"))
+(define scribblings '(("scribblings/dotenv.scrbl" ())))
+(define pkg-desc "Description Here")
+(define version "0.0")
+(define pkg-authors '(royall))
---
+++ scribblings/dotenv.scrbl
@@ -0,0 +1,10 @@
+#lang scribble/manual
+@require[@for-label[dotenv
+                    racket/base]]
+
+@title{dotenv}
+@author{royall}
+
+@defmodule[dotenv]
+
+Package Description Here
