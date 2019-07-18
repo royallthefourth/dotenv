049c834d9a923b5b4c8d39c34c1e5b14a8f725e1
Ethan Estrada <ethan@misterfidget.com>
2019-07-17 18:20:52 -0600

    Deduplicate `dotenv-read` code
    Implement by having `dotenv-read` call `dotenv-load!` while
    parameterizing `current-environment-variables` with a fresh
    `environment-variables?` set, then return the new set. Flesh out
    testing to make sure this doesn't break anything.

--- private/dotenv.rkt
+++ private/dotenv.rkt
@@ -42,17 +42,9 @@
       (load-files (cdr files) (load-file (car files) vars))))
 
 (define (dotenv-read . filenames)
-  (define files (cond ((empty? filenames) '(".env"))
-                      ((list? (car filenames)) (car filenames))
-                      (else filenames)))
-  (define new-env (make-environment-variables))
-  (map
-   (λ (pair)
-     (environment-variables-set! new-env
-                                 (string->bytes/locale (car pair))
-                                 (string->bytes/locale (cdr pair))))
-   (load-files files '()))
-  new-env)
+  (parameterize ([current-environment-variables (make-environment-variables)])
+    (apply dotenv-load! filenames)
+    (current-environment-variables)))
 
 (define (dotenv-load! . filenames)
   (define files (cond ((empty? filenames) '(".env"))
@@ -78,26 +70,66 @@
   (check-equal? (car (process-file envstream '())) test-pass)
   (check-equal? (car (load-file ".env" '())) test-pass)
 
-  (parameterize ((current-environment-variables (make-environment-variables)))
-    (check-equal? (getenv (car test-user)) #f)
-    (dotenv-load!)
-    (check-equal? (getenv (car test-user)) (cdr test-user))
+  (parameterize ([current-environment-variables (make-environment-variables)])
+    (test-case
+      "Test that dotenv-load! changes environment"
+      (check-false (getenv (car test-user))
+        "Environment not clear before main check")
+      (dotenv-load!)
+      (check-equal? (getenv (car test-user)) (cdr test-user)
+        "Environment did not change after call to dotenv-load!"))
 
-    (check-equal? (getenv (car more-user)) #f)
-    (dotenv-load! "more.env")
-    (check-equal? (getenv (car more-user)) (cdr more-user))
+    (test-case
+      "Test that dotenv-load! changes environment with explicit filename"
+      (check-false (getenv (car more-user))
+        "Environment not clear before main check")
+      (dotenv-load! "more.env")
+      (check-equal? (getenv (car more-user)) (cdr more-user)
+        "Environment did not change after call to dotenv-load!"))
 
-    (check-equal? (getenv (car most-user)) #f)
-    (dotenv-load! '("most.env"))
-    (check-equal? (getenv (car most-user)) (cdr most-user)))
+    (test-case
+      "Test that dotenv-load! changes environment with list of filenames"
+      (check-false (getenv (car most-user))
+        "Environment not clear before main check")
+      (dotenv-load! '("most.env"))
+      (check-equal? (getenv (car most-user)) (cdr most-user)
+        "Environment did not change after call to dotenv-load!")))
 
-  (check-equal?
-   (environment-variables-ref
-    (dotenv-read)
-    (string->bytes/utf-8 (car test-user)))
-   (string->bytes/utf-8 (cdr test-user)))
-   (check-equal?
-    (environment-variables-ref
-     (dotenv-read "more.env")
-     (string->bytes/utf-8 (car more-user)))
-    (string->bytes/utf-8 (cdr more-user))))
+  (test-case
+    "Test that dotenv-read does not change environment"
+    (check-false (getenv (car test-user))
+      "Environment not clear before main check")
+    (define test-env (dotenv-read))
+    (check-false (getenv (car test-user))
+      "Environment should not be affected by call to dotenv-read")
+    (check-equal?
+      (environment-variables-ref
+        test-env
+        (string->bytes/utf-8 (car test-user)))
+      (string->bytes/utf-8 (cdr test-user))))
+
+  (test-case
+    "Test that dotenv-read does not change environment with explicit filename"
+    (check-false (getenv (car more-user))
+      "Environment not clear before main check")
+    (define test-env2 (dotenv-read "more.env"))
+    (check-false (getenv (cdr more-user))
+      "Environment should not be affected by call to dotenv-read")
+    (check-equal?
+      (environment-variables-ref
+        test-env2
+        (string->bytes/utf-8 (car more-user)))
+      (string->bytes/utf-8 (cdr more-user))))
+
+  (test-case
+    "Test that dotenv-read does not change environment with list of filenames"
+    (check-false (getenv (car most-user))
+      "Environment not clear before main check")
+    (define test-env3 (dotenv-read '("most.env")))
+    (check-false (getenv (cdr most-user))
+      "Environment should not be affected by call to dotenv-read")
+    (check-equal?
+      (environment-variables-ref
+        test-env3
+        (string->bytes/utf-8 (car most-user)))
+      (string->bytes/utf-8 (cdr most-user)))))
